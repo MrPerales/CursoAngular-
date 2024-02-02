dcd895bb5fef06b6929293f1734def6214f88b97
Carlos Perales <carlosperales050@gmail.com>
2024-02-01 18:00:30 -0600

    render array  list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'todoApp';
+  welcome = 'welcome to app';
+  tasks = ['instalar Angular CLI', 'crear Proyecto ', 'crear compnentes'];
 }
