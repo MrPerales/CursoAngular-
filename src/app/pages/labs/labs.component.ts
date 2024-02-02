dd32d254d6b0c170a41f1fda2d41b480b08bca82
Carlos Perales <carlosperales050@gmail.com>
2024-02-02 15:19:59 -0600

    add :signals

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -11,7 +11,8 @@ import { CommonModule } from '@angular/common';
 export class LabsComponent {
   welcome = 'welcome to app';
   tasks = ['instalar Angular CLI', 'crear Proyecto ', 'crear compnentes'];
-  name = 'Carlos';
+  name = signal('Carlos');
+  contador = signal(0);
   image =
     'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/1200px-GitHub_Invertocat_Logo.svg.png';
   disabled = true;
@@ -36,5 +37,13 @@ export class LabsComponent {
   handlerKeydown(event: KeyboardEvent) {
     const input = event.target as HTMLInputElement;
     console.log(input.value);
+  }
+  handlerName(event: KeyboardEvent) {
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.name.set(newValue);
+  }
+  increases() {
+    this.contador.update((value) => value + 1);
   }
 }
